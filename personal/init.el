16f33fc7aa0f25b609ac0168f90e690dbdcf6a86
Liam Bowen <LiamBowen@gmail.com>
2015-11-11 15:44:34 -0500

    magit-push-always-verify = so irritating

--- personal/init.el
+++ personal/init.el
@@ -1,3 +1,7 @@
+;;; personal-init --- My personal initialization file
+;;; Commentary:
+;;; this linter is very pedantic
 ;;; Code:
 (set-default 'truncate-lines t)
 (global-set-key (kbd "C-^") 'mc/edit-lines)
+(setq magit-push-always-verify nil)
