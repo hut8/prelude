91065176cb25980885497a426ef66f0e8572f8d4
Liam Bowen <LiamBowen@gmail.com>
2016-01-27 19:29:09 -0500

    defvars, settings + editorconfig

--- personal/init.el
+++ personal/init.el
@@ -2,7 +2,17 @@
 ;;; Commentary:
 ;;; this linter is very pedantic
 ;;; Code:
+(prelude-require-packages '(multiple-cursors))
 (set-default 'truncate-lines t)
 (global-set-key (kbd "C-^") 'mc/edit-lines)
+(defvar magit-push-always-verify)
+(defvar org-agenda-files)
+(defvar prelude-guru)
+(defvar prelude-flyspell)
 (setq magit-push-always-verify nil)
+(setq org-agenda-files '("~/Dropbox/org"))
+(setq prelude-guru nil)
 (setq prelude-flyspell nil)
+(editorconfig-mode 1)
+(provide 'init)
+;;; init.el ends here
