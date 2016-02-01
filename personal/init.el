84411b6b5525dee61878e0bc77f5b59e15bdfd75
Liam Bowen <liambowen@gmail.com>
2016-02-01 16:14:10 -0500

    Windows-specific stuff

--- personal/init.el
+++ personal/init.el
@@ -7,12 +7,23 @@
 (global-set-key (kbd "C-^") 'mc/edit-lines)
 (defvar magit-push-always-verify)
 (defvar org-agenda-files)
+(defvar org-log-done)
+(defvar org-html-doctype)
 (defvar prelude-guru)
 (defvar prelude-flyspell)
+
 (setq magit-push-always-verify nil)
+
 (setq org-agenda-files '("~/Dropbox/org"))
+(setq org-log-done t)
+(setq org-html-doctype "html5")
+
 (setq prelude-guru nil)
 (setq prelude-flyspell nil)
 (editorconfig-mode 1)
+(add-hook 'web-mode-hook
+          (lambda () (whitespace-mode -1)))
+
 (provide 'init)
+
 ;;; init.el ends here
