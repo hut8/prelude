080cffbe39011ab2f2a0c79f27c1c22f6ec96622
Liam Bowen <liam@tenex.tech>
2016-10-24 01:02:14 -0400

    php fixen; org fix

--- personal/init.el
+++ personal/init.el
@@ -15,12 +15,19 @@
 (defvar web-mode-engines-alist)
 (setq auto-save-default nil)
 (setq magit-push-always-verify nil)
-(setq org-agenda-files '("~/Dropbox/org"))
+(setq org-agenda-files '("~/org"))
 (setq org-log-done t)
 (setq org-html-doctype "html5")
 (setq prelude-guru nil)
 (setq prelude-flyspell nil)
+(setq tab-width 4)
 (editorconfig-mode 1)
+
+(add-hook 'php-mode-hook 'my-php-mode-hook)
+(defun my-php-mode-hook ()
+  "PHP is terrible."
+  (setq indent-tabs-mode t))
+
 (add-hook 'web-mode-hook
           (lambda () (whitespace-mode -1)))
 (defun my-gfm-mode-hook ()
