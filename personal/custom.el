d1f442ee5779eab9e967ea779f4f6403ac377c45
Liam Bowen <liam@tenex.tech>
2016-12-19 16:08:07 -0500

    check in custom.el

---
+++ personal/custom.el
@@ -0,0 +1,31 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cperl-indent-parens-as-block t)
+ '(css-indent-offset 4)
+ '(expand-region-preferred-python-mode (quote fgallina-python))
+ '(indent-tabs-mode nil)
+ '(js-indent-level 2)
+ '(js2-basic-offset 2)
+ '(json-reformat:indent-width 2)
+ '(nxml-child-indent 4)
+ '(package-selected-packages
+   (quote
+    (ncl-mode zop-to-char zenburn-theme yari yaml-mode which-key web-mode web-beautify volatile-highlights vmd-mode vlf undo-tree typescript-mode systemd smex smartrep smartparens smart-mode-line slim-mode sed-mode scss-mode scala-mode2 sass-mode ruby-tools rubocop rainbow-mode rainbow-delimiters py-autopep8 php-mode ov operate-on-number nginx-mode multiple-cursors move-text markdown-mode magit json-mode jscs js2-mode inf-ruby imenu-anywhere ido-ubiquitous helm-systemd helm-pydoc helm-projectile helm-descbinds helm-ag haskell-mode guru-mode grizzl gotest god-mode go-projectile gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck-typescript-tslint flx-ido feature-mode expand-region elisp-slime-nav editorconfig-core editorconfig easy-kill discover-my-major diminish diff-hl csv-mode crux company-go company-anaconda coffee-mode browse-kill-ring beacon apache-mode anzu ansible-doc ansible alchemist ace-window)))
+ '(python-shell-interpreter "python3")
+ '(show-trailing-whitespace t)
+ '(tab-always-indent (quote complete))
+ '(tab-width 4)
+ '(typescript-indent-level 2)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-enable-auto-closing t)
+ '(web-mode-enable-auto-indentation t)
+ '(web-mode-markup-indent-offset 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
